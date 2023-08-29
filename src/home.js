ee76d493b2e68e8b3c3cab5f7279f99d853f7a78
Enverhann <enverhanoksuz@hotmail.com>
2023-08-29 23:47:48 +0300

    image folder moved

--- src/home.js
+++ src/home.js
@@ -6,7 +6,7 @@ export function loadHomePage() {
     homeDiv.classList.add('tab-content');
     homeDiv.innerHTML = `
     <div class="centered-content">
-        <img src="../src/images/restaurant-image.jpg" alt="Restaurant Image" class="centered-image">
+        <img src="images/restaurant-image.jpg" alt="Restaurant Image" class="centered-image">
         <p>Experience the finest dining in town at our restaurant. We offer a wide range of delicious dishes prepared with the freshest ingredients. Join us for an unforgettable culinary journey.</p>
     </div>
 `;
--- src/menu.js
+++ src/menu.js
@@ -9,7 +9,7 @@ export function loadMenuPage() {
     const foodItem1 = document.createElement('li');
     foodItem1.innerHTML =  `   
     <div class="menu-item">
-        <img src="../src/images/spaghetti-carbonara.webp" alt="Spaghetti Carbonara" class="food-image">
+        <img src="images/spaghetti-carbonara.webp" alt="Spaghetti Carbonara" class="food-image">
         <div class="menu-description">
             <h3>Spaghetti Carbonara</h3>
             <p>Consisting of spaghetti (long thin strands of pasta) with bacon and a creamy sauce made from eggs, Pecorino or Parmesan, and black pepper.</p>
@@ -21,7 +21,7 @@ export function loadMenuPage() {
     const foodItem2 = document.createElement('li');
     foodItem2.innerHTML =   ` 
     <div class="menu-item">
-        <img src="../src/images/margherita-pizza.jpeg" alt="Spaghetti Carbonara" class="food-image">
+        <img src="images/margherita-pizza.jpeg" alt="Spaghetti Carbonara" class="food-image">
         <div class="menu-description">
             <h3>Margherita Pizza</h3>
             <p>Margherita pizza features a bubbly crust, crushed San Marzano tomato sauce, fresh mozzarella and basil, a drizzle of olive oil, and a sprinkle of salt.</p>
@@ -32,7 +32,7 @@ export function loadMenuPage() {
     const foodItem3 = document.createElement('li');
     foodItem3.innerHTML =
     `<div class="menu-item">
-        <img src="../src/images/grilled-salmon.jpeg" alt="Grilled Salmon" class="food-image">
+        <img src="images/grilled-salmon.jpeg" alt="Grilled Salmon" class="food-image">
         <div class="menu-description">
             <h3>Grilled Salmon</h3>
             <p>Grilled salmon is salmon that has been cooked over coals or a gas flame and is one of the oldest fish preparations known.</p>
@@ -43,7 +43,7 @@ export function loadMenuPage() {
     const foodItem4 = document.createElement('li');
     foodItem4.innerHTML =
     `<div class="menu-item">
-        <img src="../src/images/caesar-salad.jpeg" alt="Caesar Salad" class="food-image">
+        <img src="images/caesar-salad.jpeg" alt="Caesar Salad" class="food-image">
         <div class="menu-description">
             <h3>Caesar Salad</h3>
             <p>Caesar salad is made with romaine lettuce, croutons, Parmesan cheese, and Caesar dressing.</p>
@@ -54,7 +54,7 @@ export function loadMenuPage() {
     const foodItem5 = document.createElement('li');
     foodItem5.innerHTML =
     `<div class="menu-item">
-        <img src="../src/images/beef-burger.webp" alt="Beef Burger" class="food-image">
+        <img src="images/beef-burger.webp" alt="Beef Burger" class="food-image">
         <div class="menu-description">
             <h3>Beef Burger</h3>
             <p>The classic burger is an all-time BBQ favorite! This super easy homemade beef burger recipe gives you delicious patties, packed with onions and herbs for extra flavor, that are perfect for topping with cheese, lettuce, tomato, and sandwiching between floury buns.</p>
@@ -65,7 +65,7 @@ export function loadMenuPage() {
     const foodItem6 = document.createElement('li');
     foodItem6.innerHTML =
     `<div class="menu-item">
-        <img src="../src/images/vegetable-stir-fry.jpeg" alt="Vegetable Stir Fry" class="food-image">
+        <img src="images/vegetable-stir-fry.jpeg" alt="Vegetable Stir Fry" class="food-image">
         <div class="menu-description">
             <h3>Vegetable Stir Fry</h3>
             <p>Vegetable Stir Fry is a mixture of colorful vegetables sautéed in a sweet and savory sauce that makes for a simple weeknight meal!</p>
